a38a171c10a93566f915d47e2d94a27d63ef8987
NandhuKrishhhna <nandhu.krishna@wavenetsolutions.in>
2025-10-07 18:10:13 +0530

    starting with basic crud operations

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI
-
-
-
+from fastapi import FastAPI, Header
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
+
+class CreateBookModel(BaseModel):
+    title:str
+    author:str
+    rating:float
 
 @app.get('/')
 async def read_root():
     return {
         "message":"Hello world"
     }
+@app.get("/greet")   
+async def greet_name(name:Optional[str] = "User", age : int =0) -> dict:
+    return {"message":f"Hello {name} age : {age}"}
+
+@app.post("/create-book")
+async def create_book(book_data:CreateBookModel):
+    return {
+        "title":book_data.title,
+        "author":book_data.author,
+        "rating":book_data.rating
+    }
+
+@app.get("/get_headers")
+async def get_header(
+    accept:str = Header(None),
+    content_type :str = Header(None),
+    user_agent:str= Header(None),
+    host:str = Header(None)
+):
+    request_header = {}
+    request_header["Accept"] = accept
+    request_header["Content-type"] = content_type
+    request_header["User-agent"] = user_agent
+    request_header["Host"] = host
+    
+    return request_header
+    
