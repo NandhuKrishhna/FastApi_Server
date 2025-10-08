d93436022e2fe4a38948aa37f034e8307d6b4704
NandhuKrishhhna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-10-08 11:48:54 +0530

    organizing routes

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,25 +1,22 @@
-from fastapi import FastAPI , status , HTTPException
+from fastapi import APIRouter,status,HTTPException
 from typing import List
+from src.books.schema import BookCreateModel, BookModel
 from src.books.book_collection import books
-from src.books.schema import BookModel,BookCreateModel
+book_router = APIRouter()
 
-
-app = FastAPI()
-
-
-@app.get('/books', response_model = List[BookModel])
+@book_router.get('/', response_model = List[BookModel])
 async def get_all_books():
     return books
 
 
-@app.post('/books',status_code=status.HTTP_201_CREATED , response_model=BookCreateModel)
+@book_router.post('/',status_code=status.HTTP_201_CREATED , response_model=BookCreateModel)
 async def create_book(book_data: BookCreateModel):
     new_id = books[-1]["id"] + 1 if books else 1
     new_book ={"id":new_id,**book_data.model_dump()}
     books.append(new_book)
     return new_book
 
-@app.get('/book/{book_id}', response_model=BookModel)
+@book_router.get('/{book_id}', response_model=BookModel)
 async def get_book(book_id:int):
     for book in books:
 
@@ -28,7 +25,7 @@ async def get_book(book_id:int):
             return book
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found.")    
     
-@app.delete('/book/{book_id}', status_code=status.HTTP_200_OK)
+@book_router.delete('/{book_id}', status_code=status.HTTP_200_OK)
 async def delete_book(book_id: int):
     for index, book in enumerate(books):
         if book["id"] == book_id:
